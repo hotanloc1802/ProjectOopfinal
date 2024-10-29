d91d85c40ea5db5bfd971463231ff50f835393cb
Loc <trantanloc20045@gmail.com>
2024-10-30 05:13:11 +0700

    Test conflict 2

---
+++ ClassroomManagementApp1/Views/Class3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomManagementApp1.Views
+{
+    internal class Class3
+    {
+        int a;
+    }
+}
